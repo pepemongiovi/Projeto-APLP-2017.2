2d01ef9c41a0c80be5f2931589dbebae4ca81b34
Giuseppe <giuseppe.mongiovi@ccc.ufcg.edu.br>
2017-11-16 01:38:48 -0300

    modo singleplayer adicionado

--- projetoAPLP.cpp
+++ projetoAPLP.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-const int ALTURA = 6, LARGURA = 11, MAX_SHIFT = 3, MIN_SHIFT = -3;
+const int ALTURA = 6, LARGURA = 11, SINGLEPLAYER = 0, MAX_SHIFT = 3, MIN_SHIFT = -3;
 
 const int REDE = 0, TRAVE_CIMA = -1, TRAVE_LADO = -2, CABECA = 1, BRACO_ESQUERDO = 2, BRACO_DIREITO = 3, 
 			CORPO = 4, PERNA_ESQUERDA = 5, PERNA_DIREITA = 6, BOLA = 7;
@@ -11,6 +11,7 @@ const int ESQUERDA_CIMA = 1, ESQUERDA_BAIXO = 2, MEIO_CIMA = 3,
 		MEIO_BAIXO = 4, DIREITA_CIMA = 5, DIREITA_BAIXO = 6;
 		  
 map<int, char> chars;
+int modoDePartida;
 
 void buildMap()
 {
@@ -80,6 +81,20 @@ void exibirOpcoes()
 	cout<<"4 - Meio baixo"<<endl;
 	cout<<"5 - Direita cima"<<endl;
 	cout<<"6 - Direita baixo\n"<<endl;
+}
+
+void exibirMenu()
+{
+	cout<<"################"<<endl;
+	cout<<"# MENU INICIAL #"<<endl;
+	cout<<"################\n"<<endl;
+	
+	cout<<"0 - SINGLEPLAYER"<<endl;
+	cout<<"1 - MULTIPLAYER\n"<<endl;
+	
+	cout<<"Escolha um modo de partida: ";	
+	cin >> modoDePartida;
+	cout<<endl;
 }
 
 bool isGol(int localDoChute)
@@ -163,32 +178,93 @@ void realizarJogada(int &p, int jogador)
 	}	
 }
 
+void realizarJogadaPC(int &p, int jogador)
+{
+	cout<<"JOGADOR "<<jogador<<endl;
+	cout<<"=========="<<endl;
+	montarTrave(0);
+	exibirTrave();
+	exibirOpcoes();
+	
+	int localDoChute;
+	
+	localDoChute = 1 + (rand() % static_cast<int>(6 - 1 + 1));
+	
+	srand(time(NULL));
+	int shift = MIN_SHIFT + (rand() % static_cast<int>(MAX_SHIFT - MIN_SHIFT + 1));
+	montarTrave(shift);
+	
+	if(isGol(localDoChute))
+	{
+		exibirTrave();
+		cout<<"GOLL!!\n"<<endl;
+		p++;
+	}
+	else
+	{
+		exibirTrave();
+		cout<<"ERROU!!\n"<<endl;
+	}	
+}
+
 int main()
 {	
 	buildMap();
 	
-	int p1 =0 , p2 = 0, rodadas = 0;
+	exibirMenu();
 	
-	while((rodadas<5 and abs(p1-p2<3)) or (rodadas>=5 and p1==p2))
+	if(modoDePartida==SINGLEPLAYER)
 	{
-		cout<<"############"<<endl;
-		cout<<"#RODADA "<<rodadas+1<<"!!#"<<endl;
-		cout<<"############\n"<<endl;
+		int p1 =0 , pc = 0, rodadas = 0;
+	
+		while((rodadas<5 and abs(p1-pc<3)) or (rodadas>=5 and p1==pc))
+		{
+			cout<<"############"<<endl;
+			cout<<"#RODADA "<<rodadas+1<<"!!#"<<endl;
+			cout<<"############\n"<<endl;
+			
+			exibirPlacar(p1,pc);
+			realizarJogada(p1, 1);
+			realizarJogadaPC(pc, 2);
+			rodadas++;	
+		}
 		
-		exibirPlacar(p1,p2);
-		realizarJogada(p1, 1);
-		realizarJogada(p2, 2);
-		rodadas++;	
-	}
-	
-	cout<< "FINAL DE PARTIDA!!\n"<<endl;
-	exibirPlacar(p1,p2);
-	if(p1>p2)
-	{
-		cout<< "O VENCEDOR FOI O JOGADOR 1!"<<endl;
+		cout<< "FINAL DE PARTIDA!!\n"<<endl;
+		exibirPlacar(p1,pc);
+		if(p1>pc)
+		{
+			cout<< "VOCE VENCEU!"<<endl;
+		}
+		else
+		{
+			cout<< "NAO FOI DESSA VEZ.."<<endl;
+		}
 	}
 	else
 	{
-		cout<< "O VENCEDOR FOI O JOGADOR 2!"<<endl;
-	}
+		int p1 =0 , p2 = 0, rodadas = 0;
+	
+		while((rodadas<5 and abs(p1-p2<3)) or (rodadas>=5 and p1==p2))
+		{
+			cout<<"############"<<endl;
+			cout<<"#RODADA "<<rodadas+1<<"!!#"<<endl;
+			cout<<"############\n"<<endl;
+			
+			exibirPlacar(p1,p2);
+			realizarJogada(p1, 1);
+			realizarJogada(p2, 2);
+			rodadas++;	
+		}
+		
+		cout<< "FINAL DE PARTIDA!!\n"<<endl;
+		exibirPlacar(p1,p2);
+		if(p1>p2)
+		{
+			cout<< "O JOGADOR 1 VENCEU!"<<endl;
+		}
+		else
+		{
+			cout<< "O JOGADOR 2 VENCEU!"<<endl;
+		}
+	}	
 }
